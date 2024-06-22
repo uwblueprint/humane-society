a440a50b648fec7604c4f762bdd86bc4dc5e319b
Trinity <32817635+trinity-y@users.noreply.github.com>
2024-06-22 12:24:22 -0400

    add pet table

---
+++ backend/typescript/models/pet.model.ts
@@ -0,0 +1,55 @@
+import { Column, Model, Table, DataType, ForeignKey, BelongsTo } from "sequelize-typescript";
+import Animal_Type  from "./animal_type.model"
+//import { PetCareInfo } from "./PetCareInfo"
+
+import { Letters } from "../types";
+
+@Table({ tableName: "entities" })
+export default class Pet extends Model {
+  @Column ({ type: DataType.INTEGER })
+  id!: number;
+
+  @ForeignKey(() => Animal_Type)
+  @Column ({ type: DataType.INTEGER })
+  animal_type_id!: number;
+  
+  @BelongsTo(() => Animal_Type)
+  animal_type!: Animal_Type;   
+
+  @Column ({ type: DataType.STRING })
+  name!: string;
+
+  @Column ({ type: DataType.STRING })
+  breed!: string;
+
+  @Column ({ type: DataType.INTEGER })
+  age!: number;
+
+  @Column ({ type: DataType.BOOLEAN })
+  adoption_status!: boolean;
+
+//   @ForeignKey(() => PetCareInfo)
+//   @Column ({ type: DataType.INTEGER })
+//   pet_care_info_id?: number;
+
+//   @BelongsTo(() => PetCareInfo)
+//   petCareInfo?: PetCareInfo;
+
+  @Column ({ type: DataType.DECIMAL })
+  weight!: number;
+
+  @Column({ type: DataType.BOOLEAN })
+  spayed!: boolean;
+  
+  @Column({ type: DataType.ENUM("M", "F") })
+  sex!: Letters;
+
+  @Column({ type: DataType.STRING })
+  photo!: string;
+
+  @Column ({ type: DataType.DATE })
+  created_at!: Date;
+
+  @Column ({ type: DataType.DATE })
+  updated_at?: Date;
+}
