a07b42c98c8d794a180f26ad3ec1848764fcff9f
Daniel Zhang <z331317@gmail.com>
2024-06-30 13:31:14 -0400

    Fix login and create placeholder landing page

--- frontend/src/components/auth/Login.tsx
+++ frontend/src/components/auth/Login.tsx
@@ -1,44 +1,19 @@
 import React, { useContext, useState } from "react";
-import { Redirect, useHistory } from "react-router-dom";
-import {
-  GoogleLogin,
-  GoogleLoginResponse,
-  GoogleLoginResponseOffline,
-} from "react-google-login";
+import { Redirect } from "react-router-dom"; 
 
 import authAPIClient from "../../APIClients/AuthAPIClient";
-import { HOME_PAGE, SIGNUP_PAGE } from "../../constants/Routes";
+import { HOME_PAGE } from "../../constants/Routes";
 import AuthContext from "../../contexts/AuthContext";
 import { AuthenticatedUser } from "../../types/AuthTypes";
-
-type GoogleResponse = GoogleLoginResponse | GoogleLoginResponseOffline;
-
-type GoogleErrorResponse = {
-  error: string;
-  details: string;
-};
-
+ 
 const Login = (): React.ReactElement => {
   const { authenticatedUser, setAuthenticatedUser } = useContext(AuthContext);
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const history = useHistory();
-
+  const [password, setPassword] = useState(""); 
   const onLogInClick = async () => {
     const user: AuthenticatedUser = await authAPIClient.login(email, password);
     setAuthenticatedUser(user);
-  };
-
-  const onSignUpClick = () => {
-    history.push(SIGNUP_PAGE);
-  };
-
-  const onGoogleLoginSuccess = async (tokenId: string) => {
-    const user: AuthenticatedUser = await authAPIClient.loginWithGoogle(
-      tokenId,
-    );
-    setAuthenticatedUser(user);
-  };
+  }; 
 
   if (authenticatedUser) {
     return <Redirect to={HOME_PAGE} />;
@@ -73,32 +48,8 @@ const Login = (): React.ReactElement => {
             Log In
           </button>
         </div>
-        <GoogleLogin
-          clientId={process.env.REACT_APP_OAUTH_CLIENT_ID || ""}
-          buttonText="Login with Google"
-          onSuccess={(response: GoogleResponse): void => {
-            if ("tokenId" in response) {
-              onGoogleLoginSuccess(response.tokenId);
-            } else {
-              // eslint-disable-next-line no-alert
-              window.alert(response);
-            }
-          }}
-          onFailure={(error: GoogleErrorResponse) =>
-            // eslint-disable-next-line no-alert
-            window.alert(JSON.stringify(error))
-          }
-        />
-      </form>
-      <div>
-        <button
-          className="btn btn-primary"
-          type="button"
-          onClick={onSignUpClick}
-        >
-          Sign Up
-        </button>
-      </div>
+         
+      </form> 
     </div>
   );
 };
---
+++ frontend/src/components/pages/LandingPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const GetPage = (): React.ReactElement => {
+  return (
+    <div style={{ textAlign: "center", width: "25%", margin: "0px auto" }}>
+      <h1>Pets</h1> 
+    </div>
+  );
+};
+
+export default GetPage;
