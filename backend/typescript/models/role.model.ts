0925e8dbb56e55d9edfe3844794fe3488ef33568
Jerry Cheng <j983chen@uwaterloo.ca>
2024-07-02 22:26:44 -0400

    removed timestamps from roles table

--- backend/typescript/models/role.model.ts
+++ backend/typescript/models/role.model.ts
@@ -1,6 +1,6 @@
 import { Column, DataType, Model, Table } from "sequelize-typescript";
 
-@Table({ tableName: "roles" })
+@Table({ tableName: "roles", timestamps: false })
 export default class Role extends Model {
   @Column({ type: DataType.STRING })
   role_name!: string;
