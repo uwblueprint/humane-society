08add0a9dbdfcfd0355c12395531fde9d2f3e0d0
Trinity <32817635+trinity-y@users.noreply.github.com>
2024-07-02 21:35:45 -0400

    format animaltype

--- backend/typescript/models/animalType.model.ts
+++ backend/typescript/models/animalType.model.ts
@@ -1,24 +1,7 @@
-import {
-  Column,
-  Model,
-  Table,
-  DataType,
-  PrimaryKey,
-  AutoIncrement,
-} from "sequelize-typescript";
+import { Column, Model, Table } from "sequelize-typescript";
 
 @Table({ timestamps: false, tableName: "animal_types" })
 export default class AnimalType extends Model {
-  @PrimaryKey
-  @AutoIncrement
-  @Column({
-    type: DataType.INTEGER,
-  })
-  id!: number;
-
-  @Column({
-    type: DataType.STRING,
-    allowNull: false,
-  })
+  @Column({})
   animal_type_name!: string;
 }
