22040af71d68eadb99ad0dc9ba707b7bc84213f8
Trinity <32817635+trinity-y@users.noreply.github.com>
2024-07-16 21:08:21 -0400

    add space for linter

--- backend/typescript/models/activity.model.ts
+++ backend/typescript/models/activity.model.ts
@@ -1,6 +1,6 @@
 import { Column, Model, Table } from "sequelize-typescript";
 
-@Table({ timestamps:false, tableName: "activities" })
+@Table({ timestamps: false, tableName: "activities" })
 export default class Activity extends Model {
   @Column
   activity_name!: string;
