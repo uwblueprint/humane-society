9ac5938241ebf2cd74ad3231d6b0ee4d44ea9317
laks0407 <laks0407@gmail.com>
2024-07-20 13:18:55 -0400

    adjusted routes

--- backend/typescript/rest/petRoutes.ts
+++ backend/typescript/rest/petRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { petRequestDtoValidators, petFilterValidators } from "../middlewares/validators/petValidators";
+import { petRequestDtoValidators } from "../middlewares/validators/petValidators";
 import PetService from "../services/implementations/petService";
 import {
   PetResponseDTO,
@@ -10,6 +10,71 @@ import { sendResponseByMimeType } from "../utilities/responseUtil";
 
 const petRouter: Router = Router();
 const petService: IPetService = new PetService();
+
+/* Create Behaviour */
+petRouter.post("/", petRequestDtoValidators, async (req, res) => {
+  try {
+    const { body } = req;
+    const newPet = await petService.createPet({
+        animalTypeId: body.animalTypeId,
+        name: body.name,
+        status: body.status,
+        breed: body.breed,
+        age: body.age,
+        adoptionStatus: body.adoptionStatus,
+        weight: body.weight,
+        neutered: body.neutered,
+        sex: body.sex,
+        photo: body.photo,
+        careInfo: {
+          safetyInfo: body.PetCareInfo.safety_info,
+          medicalInfo: body.PetCareInfo.medical_info,
+          managementInfo: body.PetCareInfo.management_info,
+        },
+    });
+    res.status(201).json(newPet);
+  } catch (e: unknown) {
+    // replace with Jerry's server error
+    res.status(500).send("Internal server error occured.");
+  }
+});
+
+/* Get all Behaviours */
+petRouter.get("/", async (req, res) => {
+  const contentType = req.headers["content-type"];
+  try {
+    const pets = await petService.getPets();
+    await sendResponseByMimeType<PetResponseDTO>(
+      res,
+      200,
+      contentType,
+      pets,
+    );
+  } catch (e: unknown) {
+    await sendResponseByMimeType(res, 500, contentType, [
+      {
+        error: "Internal server error occured.",
+      },
+    ]);
+  }
+});
+
+/* Get Behaviour by id */
+petRouter.get("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const pet = await petService.getPet(id);
+    res.status(200).json(pet);
+  } catch (e: unknown) {
+    if (e instanceof NotFoundError) {
+      res.status(404).send(getErrorMessage(e));
+    } else {
+      // jerry internal server error
+      res.status(500).send("Internal server error occured.");
+    }
+  }
+});
 
 /* Update Pet by id */
 petRouter.put("/:id", petRequestDtoValidators, async (req, res) => {
@@ -59,26 +124,6 @@ petRouter.delete("/:id", async (req, res) => {
   }
 });
 
-/*Get Pets by filter criteria */
-petRouter.get("/filter", petFilterValidators, async (req, res) => {
-  const { query } = req;
-    const contentType = req.headers["content-type"];
-    try {
-      const pets = await petService.filterPets(query);
-      await sendResponseByMimeType<PetResponseDTO>(
-        res,
-        200,
-        contentType,
-        pets,
-      );
-    } catch (e: unknown) {
-      await sendResponseByMimeType(res, 500, contentType, [
-        {
-          error: "Internal server error occured.",
-        },
-      ]);
-    }
-  });
   
 
 export default petRouter;
