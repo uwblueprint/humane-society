962c0dd91d6d02d73cea3d113e7a0965c3766308
Trinity <32817635+trinity-y@users.noreply.github.com>
2024-07-29 13:29:58 -0400

    rename logo

--- frontend/src/components/common/navbar/Logo.tsx
+++ frontend/src/components/common/navbar/Logo.tsx
@@ -6,8 +6,8 @@ const Logo = (): React.ReactElement => {
     <Box mr="1.63rem" ml={{ base: "0.03rem", md: "0.75rem" }}>
       <Image
         borderRadius="full"
-        boxSize={{ base: "2.1875rem", md: "5rem" }}
-        src="images/humanesocietylogo.png"
+        boxSize={{ base: "2.9rem", md: "5rem" }}
+        src="images/humane_society_logo.png"
         alt="Logo"
       />
     </Box>
