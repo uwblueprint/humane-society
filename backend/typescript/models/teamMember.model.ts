ed40db4f40d20ce3987de12c5e4498f1e08f0073
Justin Lau <jlau@uwaterloo.ca>
2024-09-26 21:55:15 -0400

    fix formatting

--- backend/typescript/models/teamMember.model.ts
+++ backend/typescript/models/teamMember.model.ts
@@ -1,22 +1,24 @@
 import {
-    Column,
-    DataType,
-    Model,
-    Table,
-    AllowNull,
-  } from "sequelize-typescript";
-  import { TeamRole } from "../types";
-  
-  @Table({ timestamps: false, tableName: "team_members" })
-  export default class User extends Model {
-    @Column({ type: DataType.STRING, allowNull: false })
-    firstName!: string;
-  
-    @Column({ type: DataType.STRING, allowNull: false })
-    lastName!: string;
-  
-    @AllowNull(false)
-    @Column({ type: DataType.ENUM("PM", "DESIGNER", "PL", "DEVELOPER"), allowNull: false })
-    teamRole!: TeamRole;
-  }
-  
+  Column,
+  DataType,
+  Model,
+  Table,
+  AllowNull,
+} from "sequelize-typescript";
+import { TeamRole } from "../types";
+
+@Table({ timestamps: false, tableName: "team_members" })
+export default class User extends Model {
+  @Column({ type: DataType.STRING, allowNull: false })
+  firstName!: string;
+
+  @Column({ type: DataType.STRING, allowNull: false })
+  lastName!: string;
+
+  @AllowNull(false)
+  @Column({
+    type: DataType.ENUM("PM", "DESIGNER", "PL", "DEVELOPER"),
+    allowNull: false,
+  })
+  teamRole!: TeamRole;
+}
--- backend/typescript/services/interfaces/teamMemberService.ts
+++ backend/typescript/services/interfaces/teamMemberService.ts
@@ -1,20 +1,20 @@
 import { TeamMemberDTO, CreateTeamMemberDTO } from "../../types";
-      
-interface ITeamMemberService {
-    /**
-     * retrieve all team members in the array
-     * @returns all team members
-     * @throws Error if retrieval fails
-     */
-    getTeamMembers(): Promise<TeamMemberDTO[]>;
 
-    /**
-     * retrieve the team member with the given id
-     * @param teamMember team member id
-     * @returns created team member
-     * @throws Error if retrieval fails
-     */
-    createTeamMember(teamMember: CreateTeamMemberDTO): Promise<TeamMemberDTO>;
+interface ITeamMemberService {
+  /**
+   * retrieve all team members in the array
+   * @returns all team members
+   * @throws Error if retrieval fails
+   */
+  getTeamMembers(): Promise<TeamMemberDTO[]>;
+
+  /**
+   * retrieve the team member with the given id
+   * @param teamMember team member id
+   * @returns created team member
+   * @throws Error if retrieval fails
+   */
+  createTeamMember(teamMember: CreateTeamMemberDTO): Promise<TeamMemberDTO>;
 }
 
 export default ITeamMemberService;
