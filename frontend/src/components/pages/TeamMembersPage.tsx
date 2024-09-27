b041b71f873d32003c9139116649179a1930cdc2
Justin Lau <jlau@uwaterloo.ca>
2024-09-26 21:40:55 -0400

    Made frontend

--- frontend/src/components/pages/TeamMembersPage.tsx
+++ frontend/src/components/pages/TeamMembersPage.tsx
@@ -1,41 +1,61 @@
 import React, { useEffect, useState } from "react";
 import {
-    Table, Thead, Tbody, Tr, Th, Td, TableContainer, VStack,Button} from "@chakra-ui/react";
+  Table, Text, Thead, Input, Tbody, Tr, Th, Td, TableContainer, VStack, Button
+} from "@chakra-ui/react";
 import TeamMembersAPIClient from "../../APIClients/TeamMembersAPIClient";
 import { TeamMember } from "../../types/TeamMembersTypes";
 
 const TeamMembersPage = (): React.ReactElement => {
-const [teamMembers, setTeamMembers] = useState<TeamMember[]>([]);
+  const [teamMembers, setTeamMembers] = useState<TeamMember[]>([]);
 
-const getTeamMembers = async () => {
+  const getTeamMembers = async () => {
     try {
-        const teamMember = await TeamMembersAPIClient.get();
-        if (teamMember != null) {
-            setTeamMembers(teamMember);
-        }
+      const teamMember = await TeamMembersAPIClient.get();
+      if (teamMember != null) {
+        setTeamMembers(teamMember);
+      }
     } catch (error) {
-        console.error();
+      console.error();
     }
-};
+  };
 
-const addTeamMembers = async () => {
+  const addTeamMembers = async (firstName: string, lastName: string) => {
     try {
-        await TeamMembersAPIClient.create("Justin", "Lau", "DEVELOPER");
-        await TeamMembersAPIClient.get();
+      await TeamMembersAPIClient.create(firstName, lastName, "DEVELOPER");
+      await TeamMembersAPIClient.get();
+      await getTeamMembers();
     } catch (error) {
-        console.error();
+      console.error();
     }
-};
+  };
 
-    useEffect(() => {
+  useEffect(() => {
     getTeamMembers();
   }, []);
 
+  const [firstName, setFirstName] = React.useState('')
+  const [lastName, setLastName] = React.useState('')
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => setFirstName(event.target.value)
+  const handleLastName = (event: React.ChangeEvent<HTMLInputElement>) => setLastName(event.target.value)
+
   return (
     <div style={{ textAlign: "center", width: "25%", margin: "0px auto" }}>
-        <h1>Team Members</h1>
+      <h1>Team Members</h1>
       <VStack spacing="24px" style={{ margin: "24px auto" }}>
-        <Button onClick={addTeamMembers}>Add Team Member</Button>
+        <Text fontSize='md'> Set a first and last name of a bot to add to the table: </Text>
+        <Input
+          value={firstName}
+          onChange={handleChange}
+          placeholder='Enter a first name here'
+          size='sm'
+        />
+        <Input
+          value={lastName}
+          onChange={handleLastName}
+          placeholder='Enter a last name here'
+          size='sm'
+        />
+        <Button onClick={() => addTeamMembers(firstName, lastName)}>Add Team Member</Button>
         <TableContainer>
           <Table>
             <Thead>
@@ -56,7 +76,7 @@ const addTeamMembers = async () => {
             </Tbody>
           </Table>
         </TableContainer>
-        </VStack>
+      </VStack>
     </div>
   );
 };
