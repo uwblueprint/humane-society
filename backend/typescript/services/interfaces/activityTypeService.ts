a2da5e815acc3dc7a7b151966018565eb1bd06d4
Jerry Cheng <j983chen@uwaterloo.ca>
2024-10-08 23:30:01 -0400

    changed activity to activity type

---
+++ backend/typescript/services/interfaces/activityTypeService.ts
@@ -0,0 +1,55 @@
+export interface ActivityTypeRequestDTO {
+  activityName: string;
+}
+
+export interface ActivityTypeResponseDTO {
+  id: number;
+  activityName: string;
+}
+
+export interface IActivityTypeService {
+  /**
+   * Retrieve the ActivityType with the given id.
+   * @param id ActivityType id
+   * @returns Requested ActivityType
+   * @throws Error if retrieval fails
+   */
+  getActivityType(id: string): Promise<ActivityTypeResponseDTO>;
+
+  /**
+   * Retrieve all Activity Types.
+   * @returns Returns an array of ActivityTypes
+   * @throws Error if retrieval fails
+   */
+  getActivityTypes(): Promise<ActivityTypeResponseDTO[]>;
+
+  /**
+   * Create an ActivityType with the fields given in the DTO, return created ActivityType
+   * @param activity New ActivityType
+   * @returns The created ActivityType
+   * @throws Error if creation fails
+   */
+  createActivityType(
+    activity: ActivityTypeRequestDTO,
+  ): Promise<ActivityTypeResponseDTO>;
+
+  /**
+   * Update the ActivityType with the given id with fields in the DTO, return updated ActivityType
+   * @param id ActivityType id
+   * @param activity Updated ActivityType
+   * @returns The updated ActivityType
+   * @throws Error if update fails
+   */
+  updateActivityType(
+    id: string,
+    activity: ActivityTypeRequestDTO,
+  ): Promise<ActivityTypeResponseDTO | null>;
+
+  /**
+   * Delete the ActivityType with the given id
+   * @param id ActivityType id
+   * @returns id of the ActivityType deleted
+   * @throws Error if deletion fails
+   */
+  deleteActivityType(id: string): Promise<string>;
+}
