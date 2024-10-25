7703a8d1284ec54b9230bad15f45c2d97bf6690c
vips11 <66628544+vips11@users.noreply.github.com>
2024-10-24 20:01:47 -0400

    Addressed PR comments

--- backend/typescript/migrations/2024.10.22T19.42.10.add-behaviour-columns.ts
+++ backend/typescript/migrations/2024.10.22T19.42.10.add-behaviour-columns.ts
@@ -1,17 +1,23 @@
 import { DataType } from "sequelize-typescript";
 import { Migration } from "../umzug";
 
+const TABLE_NAME = "behaviours";
+
 export const up: Migration = async ({ context: sequelize }) => {
   await sequelize
     .getQueryInterface()
-    .addColumn("behaviours", "parent_behaviour_id", {
+    .addColumn(TABLE_NAME, "parent_behaviour_id", {
       type: DataType.INTEGER,
       allowNull: true,
+      references: {
+        model: "behaviours",
+        key: "id",
+      },
     });
 };
 
 export const down: Migration = async ({ context: sequelize }) => {
   await sequelize
     .getQueryInterface()
-    .removeColumn("behaviours", "parent_behaviour_id");
+    .removeColumn(TABLE_NAME, "parent_behaviour_id");
 };
