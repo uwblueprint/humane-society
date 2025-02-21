c3a90914474c5c6ef937eaaa2eb837cefa63beee
trinity-y <tayip@uwaterloo.ca>
2025-02-20 20:31:22 -0500

    colour theme clean commit

--- frontend/src/components/pages/ProfilePage.tsx
+++ frontend/src/components/pages/ProfilePage.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Text } from "@chakra-ui/react";
 import NavBar from "../common/navbar/NavBar";
-import Logout from "../auth/Logout";
 
 const ProfilePage = (): React.ReactElement => {
   return (
@@ -10,7 +9,6 @@ const ProfilePage = (): React.ReactElement => {
       <Text textStyle="h3" mt={{ base: "6.375rem", md: "9.375rem" }}>
         Profile Page 🫨
       </Text>
-      <Logout />
     </div>
   );
 };
---
+++ frontend/src/theme/textStyles.tsx
@@ -0,0 +1,49 @@
+import "@fontsource/poppins";
+import "@fontsource/roboto";
+
+const textStyles = {
+  h1: {
+    fontFamily: "Poppins",
+    fontWeight: 600,
+    fontSize: "40px",
+  },
+  h2: {
+    fontFamily: "Poppins",
+    fontWeight: 600,
+    fontSize: "28px",
+  },
+  h2Mobile: {
+    fontFamily: "Poppins",
+    fontWeight: 600,
+    fontSize: "24px",
+  },
+  h3: {
+    fontFamily: "Poppins",
+    fontWeight: 500,
+    fontSize: "24px",
+  },
+  subheading: {
+    fontFamily: "Poppins",
+    fontWeight: 500,
+    fontSize: "20px",
+  },
+  body: {
+    fontFamily: "Roboto",
+    fontWeight: 400,
+    fontSize: "18px",
+    lineHeight: "150%",
+  },
+  bodyMobile: {
+    fontFamily: "Roboto",
+    fontWeight: 400,
+    fontSize: "16px",
+    lineHeight: "150%",
+  },
+  button: {
+    fontFamily: "Poppins",
+    fontWeight: 500,
+    fontSize: "18px",
+  },
+};
+
+export default textStyles;
