32738511c36a60623aea75cc2518e70acbe62dd8
Cynthia Shen <shencynthia04@gmail.com>
2025-02-27 22:07:47 -0500

    add redirect to home on nav bar logo

---
+++ frontend/src/components/common/navbar/NavLogo.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Box, Image } from "@chakra-ui/react";
+import { useHistory } from "react-router-dom";
+import { HOME_PAGE } from "../../../constants/Routes";
+
+const Logo = (): React.ReactElement => {
+  const history = useHistory();
+  const goToHome = () => {
+    history.push(HOME_PAGE);
+  };
+  return (
+    <Box mr="1.63rem" ml={{ base: "0.03rem", md: "0.75rem" }}>
+      <Image
+        borderRadius="full"
+        boxSize={{ base: "2.9rem", md: "5rem" }}
+        src="/images/humane_society_logo.png"
+        alt="Logo"
+        onClick={goToHome}
+        objectFit="cover"
+      />
+    </Box>
+  );
+};
+
+export default Logo;
