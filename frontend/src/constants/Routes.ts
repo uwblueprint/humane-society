ca505a49b41c2ee7dd4a974755d153ac2bc66870
Cynthia Shen <shencynthia04@gmail.com>
2025-02-27 22:28:03 -0500

    Cleanup: Reorder routes

--- frontend/src/constants/Routes.ts
+++ frontend/src/constants/Routes.ts
@@ -2,11 +2,27 @@ export const HOME_PAGE = "/";  // home is Pet List
 
 export const LOGIN_PAGE = "/login";
 
-export const SIGNUP_PAGE = "/signup";
+export const FORGOT_PASSWORD_PAGE = "/forgot-password";
 
-export const FORGOT_PASSWORD_PAGE = "/forgotPassword";
+export const INTERACTION_LOG_PAGE = "/interation-log";
+
+export const PROFILE_PAGE = "/profile";
+
+export const USER_MANAGEMENT_PAGE = "/admin/users";
+
+export const ADMIN_PAGE = "/admin";
+
+export const CREATE_PASSWORD_PAGE = "/create-password";
+
+export const DEV_UTILITY_PAGE = "/dev-utility"; // TODO: This is only here for development purposes
+
+// STARTER CODE ROUTES
+
+export const HOOKS_PAGE = "/hooks";
 
 export const EDIT_TEAM_PAGE = "/edit-team";
+
+export const SIGNUP_PAGE = "/signup";
 
 export const DISPLAY_ENTITY_PAGE = "/entity";
 
@@ -19,17 +35,3 @@ export const DISPLAY_SIMPLE_ENTITY_PAGE = "/simpleEntity";
 export const CREATE_SIMPLE_ENTITY_PAGE = "/simpleEntity/create";
 
 export const UPDATE_SIMPLE_ENTITY_PAGE = "/simpleEntity/update";
-
-export const HOOKS_PAGE = "/hooks";
-
-export const INTERACTION_LOG_PAGE = "/interation-log";
-
-export const PROFILE_PAGE = "/profile";
-
-export const DEV_UTILITY_PAGE = "/dev-utility"; // TODO: This is only here for development purposes
-
-export const USER_MANAGEMENT_PAGE = "/admin/users";
-
-export const ADMIN_PAGE = "/admin";
-
-export const CREATE_PASSWORD_PAGE = "/create-password";
