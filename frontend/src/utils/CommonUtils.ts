4f4b975a6ec2a2ae7205d7c8cd3e304db915accd
Cynthia Shen <shencynthia04@gmail.com>
2025-03-17 21:20:28 -0400

    lint

--- frontend/src/utils/CommonUtils.ts
+++ frontend/src/utils/CommonUtils.ts
@@ -4,7 +4,7 @@ import { getLocalStorageObj } from "./LocalStorageUtils";
 
 export const getCurrentUserRole = (): string | null => {
   const currentUser = getLocalStorageObj<AuthenticatedUser>(
-    AUTHENTICATED_USER_KEY
+    AUTHENTICATED_USER_KEY,
   );
   return currentUser?.role || null;
 };
