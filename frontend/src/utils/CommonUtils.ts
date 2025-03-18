b2839fff1ecf3f543d2d8da38155d70664532b3e
Cynthia Shen <105977188+shencynthia@users.noreply.github.com>
2025-03-17 21:15:12 -0400

    inline export

--- frontend/src/utils/CommonUtils.ts
+++ frontend/src/utils/CommonUtils.ts
@@ -2,11 +2,9 @@ import AUTHENTICATED_USER_KEY from "../constants/AuthConstants";
 import { AuthenticatedUser } from "../types/AuthTypes";
 import { getLocalStorageObj } from "./LocalStorageUtils";
 
-const getCurrentUserRole = (): string | null => {
+export const getCurrentUserRole = (): string | null => {
   const currentUser = getLocalStorageObj<AuthenticatedUser>(
     AUTHENTICATED_USER_KEY
   );
   return currentUser?.role || null;
 };
-
-export default getCurrentUserRole;
