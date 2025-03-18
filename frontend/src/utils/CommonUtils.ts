e928d854c6b0fe39b8232935a72b1e354ae5998c
Cynthia Shen <shencynthia04@gmail.com>
2025-03-17 21:58:16 -0400

    Revert "inline export"
    This reverts commit b2839fff1ecf3f543d2d8da38155d70664532b3e.

--- frontend/src/utils/CommonUtils.ts
+++ frontend/src/utils/CommonUtils.ts
@@ -2,9 +2,11 @@ import AUTHENTICATED_USER_KEY from "../constants/AuthConstants";
 import { AuthenticatedUser } from "../types/AuthTypes";
 import { getLocalStorageObj } from "./LocalStorageUtils";
 
-export const getCurrentUserRole = (): string | null => {
+const getCurrentUserRole = (): string | null => {
   const currentUser = getLocalStorageObj<AuthenticatedUser>(
     AUTHENTICATED_USER_KEY,
   );
   return currentUser?.role || null;
 };
+
+export default getCurrentUserRole;
