b62f8a2c7e4f77c6b03c01d6d670d630a5b9ec75
Tony Qiu <tonyqiu@Mac-mini.local>
2025-03-20 13:51:18 -0400

    remove SearchFilterExample

--- frontend/src/pages/PetListPage.tsx
+++ frontend/src/pages/PetListPage.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import SearchFilterExample from "../components/common/SearchFilterExample";
 
 const GetPage = (): React.ReactElement => {
   return (
-    <div style={{ textAlign: "center", width: "100%", margin: "0px auto" }}>
+    <div style={{ textAlign: "center", width: "25%", margin: "0px auto" }}>
       <h1>Pets</h1>
-      <SearchFilterExample />
     </div>
   );
 };
