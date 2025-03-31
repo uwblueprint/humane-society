b9a27d38a72901faccf26fce7da7398005438c63
Cynthia Shen <shencynthia04@gmail.com>
2025-03-30 20:08:59 -0400

    Add user management icons

--- frontend/src/assets/icons/index.ts
+++ frontend/src/assets/icons/index.ts
@@ -4,6 +4,15 @@ import TaskIcon from "./tasks.svg";
 import UserManagementIcon from "./user-management.svg";
 import CloseIcon from "./close.svg";
 import FilterOpenIcon from "./filter-open.svg";
+import BirdTag from "./animal-tag/bird.svg";
+import BunnyTag from "./animal-tag/bunny.svg";
+import CatTag from "./animal-tag/cat.svg";
+import DogTag from "./animal-tag/dog.svg";
+import SmallAnimalTag from "./animal-tag/small-animal.svg";
+import AdminTag from "./user-role/admin.svg";
+import BehaviouristTag from "./user-role/behaviourist.svg";
+import StaffTag from "./user-role/staff.svg";
+import VolunteerTag from "./user-role/volunteer.svg";
 
 export {
   LogIcon,
@@ -12,4 +21,13 @@ export {
   FilterOpenIcon,
   TaskIcon,
   UserManagementIcon,
+  BirdTag,
+  BunnyTag,
+  CatTag,
+  DogTag,
+  SmallAnimalTag,
+  AdminTag,
+  BehaviouristTag,
+  StaffTag,
+  VolunteerTag,
 };
