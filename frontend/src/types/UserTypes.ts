cea8ab236aaf784438d18d8f9cf5b4eb383ae9fb
sthuray <138075787+sthuray@users.noreply.github.com>
2025-03-31 21:57:24 -0400

    Assume ColorLevel optional for FE

--- frontend/src/types/UserTypes.ts
+++ frontend/src/types/UserTypes.ts
@@ -15,5 +15,5 @@ export type User = {
 
 export type CreateUserDTO = Omit<
   User,
-  "id" | "status" | "animalTags" | "profilePhoto"
+  "id" | "status" | "colorLevel" | "animalTags" | "profilePhoto"
 >;
