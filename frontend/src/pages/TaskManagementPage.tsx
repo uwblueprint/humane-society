7c5cc41a8193202d004358a6c1bf05613d47dbb6
zhusophia <iceyes123@gmail.com>
2025-06-02 15:30:22 -0400

    save

--- frontend/src/pages/TaskManagementPage.tsx
+++ frontend/src/pages/TaskManagementPage.tsx
@@ -1,13 +1,36 @@
 import React from "react";
 import MainPageButton from "../components/common/MainPageButton";
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+  VStack,
+  Button,
+  Alert,
+  AlertIcon,
+  CloseButton,
+} from "@chakra-ui/react"; // components from chakra
+
 
 const TaskManagementPage = (): React.ReactElement => {
   return (
     <div style={{ textAlign: "center", width: "25%", margin: "0px auto" }}>
       <h1>Task Management Page</h1>
-      <h1>hi</h1>
-      <h1>hi</h1>
-      <h2>Hi</h2>
+       <TableContainer>
+        <Table variant ="simple">
+          <Thead>
+          <Tr>
+            <Th>First Name</Th>
+            <Th>Last Name</Th>
+          </Tr>
+          </Thead>
+        </Table>
+
+      </TableContainer>
       <MainPageButton />
     </div>
   );
