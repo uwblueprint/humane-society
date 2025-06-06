a67a70883d44c4e6833bb58b61879f9751941cc1
ludavidca <davidjylu7@gmail.com>
2025-06-05 21:29:44 -0400

    First Iteration, not migrated yet

--- backend/typescript/types.ts
+++ backend/typescript/types.ts
@@ -83,6 +83,52 @@ export enum UserStatus {
   INACTIVE = "Inactive",
 }
 
+export enum InteractionTypeEnum {
+  // Task Status
+  TASK_ASSIGNED = "Task Assigned",
+  TASK_SELF_ASSIGNED = "Task Self-Assigned",
+  TASK_UNASSIGNED = "Task Unassigned",
+  TASK_STARTED = "Task Started",
+  TASK_RESTARTED = "Task Restarted",
+  TASK_COMPLETED = "Task Completed",
+  TASK_INCOMPLETE = "Task Incomplete",
+  TASK_INACTIVE = "Task Inactive",
+
+  // Task Details
+  TASK_DELETED = "Task Deleted",
+  TASK_ASSIGNEE_CHANGED = "Task Assignee Changed",
+  TASK_INSTRUCTIONS_CHANGED = "Task Instructions Changed",
+  TASK_START_DATE_CHANGED = "Task Start Date Changed",
+  TASK_END_DATE_CHANGED = "Task End Date Changed",
+  RECURRING_TASK_DELETED = "Recurring Task Deleted",
+  RECURRING_TASK_NAME_CHANGED = "Recurring Task Name Changed",
+  RECURRING_TASK_DAYS_CHANGED = "Recurring Task Days Changed",
+  RECURRING_TASK_CADENCE_CHANGED = "Recurring Task Cadence Changed",
+
+  // Personal Details
+  USER_NAME_CHANGED = "User Name Changed",
+  USER_COLOR_LEVEL_CHANGED = "User Color Level Changed",
+  USER_ROLE_CHANGED = "User Role Changed",
+
+  // User Details
+  USER_INVITED = "User Invited",
+  USER_DELETED = "User Deleted",
+
+  // Pet Details
+  PET_DELETED = "Pet Deleted",
+  PET_NAME_CHANGED = "Pet Name Changed",
+  PET_COLOR_LEVEL_CHANGED = "Pet Color Level Changed",
+  PET_NEUTER_STATUS_CHANGED = "Pet Neuter Status Changed",
+  PET_SAFETY_INFO_CHANGED = "Pet Safety Info Changed",
+  PET_MEDICAL_INFO_CHANGED = "Pet Medical Info Changed",
+  PET_MANAGEMENT_INFO_CHANGED = "Pet Management Info Changed",
+
+  // Task Template Details
+  TASK_TEMPLATE_DELETED = "Task Template Deleted",
+  TASK_TEMPLATE_NAME_CHANGED = "Task Template Name Changed",
+  TASK_TEMPLATE_INSTRUCTIONS_CHANGED = "Task Template Instructions Changed",
+}
+
 export enum Category {
   GAMES = "Games",
   HUSBANDRY = "Husbandry",
