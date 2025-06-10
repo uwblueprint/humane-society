2afe1c558875c621487ba5626843c1f73c226381
tonyqiu123 <tonyqiu12345@gmail.com>
2025-06-09 21:17:37 -0400

    save

--- backend/typescript/migrations/2025.02.21T02.19.04.add-category-instruction-to-activity-type.ts
+++ backend/typescript/migrations/2025.02.21T02.19.04.add-category-instruction-to-activity-type.ts
@@ -1,14 +1,14 @@
 import { DataType } from "sequelize-typescript";
 import { Migration } from "../umzug";
-import { TaskType } from "../types";
+import { Category } from "../types";
 
 const TABLE_NAME = "activity_types";
 
 export const up: Migration = async ({ context: sequelize }) => {
-  await sequelize.getQueryInterface().addColumn(TABLE_NAME, "taskType", {
-    type: DataType.ENUM(...Object.values(TaskType)),
+  await sequelize.getQueryInterface().addColumn(TABLE_NAME, "category", {
+    type: DataType.ENUM(...Object.values(Category)),
     allowNull: false,
-    defaultValue: TaskType.MISC,
+    defaultValue: Category.MISC,
   });
   await sequelize.getQueryInterface().addColumn(TABLE_NAME, "instruction", {
     type: DataType.TEXT,
@@ -17,10 +17,10 @@ export const up: Migration = async ({ context: sequelize }) => {
 };
 
 export const down: Migration = async ({ context: sequelize }) => {
-  await sequelize.getQueryInterface().removeColumn(TABLE_NAME, "taskType");
+  await sequelize.getQueryInterface().removeColumn(TABLE_NAME, "category");
   await sequelize.getQueryInterface().removeColumn(TABLE_NAME, "instruction");
-  // delete taskType enum, otherwise causes issues with migrating up, down, then up
+  // delete category enum, otherwise causes issues with migrating up, down, then up
   await sequelize
     .getQueryInterface()
-    .sequelize.query('DROP TYPE IF EXISTS "enum_task_templates_taskType";');
+    .sequelize.query('DROP TYPE IF EXISTS "enum_activity_types_category";');
 };
