831eb7cd46833f1320791f4e41e858c9f510adaa
Julia Zhu <julia.liya.zhu@gmail.com>
2025-06-10 23:43:44 -0400

    create interaction log schema

---
+++ backend/typescript/migrations/2025.04.01T01.55.12.create-interaction-log-type-table.ts
@@ -0,0 +1,28 @@
+import { DataType } from "sequelize-typescript";
+
+import { Migration } from "../umzug";
+
+const TABLE_NAME = "interaction_log_types";
+
+export const up: Migration = async ({ context: sequelize }) => {
+    await sequelize.getQueryInterface().createTable(TABLE_NAME, {
+        id: {
+            type: DataType.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        action_name: {
+            type: DataType.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: DataType.STRING,
+            allowNull: true,
+        },
+    });
+};
+
+export const down: Migration = async ({ context: sequelize }) => {
+    await sequelize.getQueryInterface().dropTable(TABLE_NAME);
+};
+
---
+++ backend/typescript/models/interactionLog.model.ts
@@ -0,0 +1,31 @@
+import { Column, Model, Table, DataType, ForeignKey } from "sequelize-typescript";
+import User from "./user.model";
+import Pet from "./pet.model";
+import Activity from "./activity.model";
+import InteractionType from "./interactionType.model";
+
+@Table({ tableName: "interaction_log", timestamps: true, createdAt: "created_at", })
+export default class InteractionLog extends Model {
+  @ForeignKey(() => User)
+  @Column({ type: DataType.INTEGER, allowNull: true })
+  actor_id: number;
+
+  @ForeignKey(() => Pet)
+  @Column({ type: DataType.INTEGER, allowNull: true })
+  target_pet_id: number;
+
+  @ForeignKey(() => User)
+  @Column({ type: DataType.INTEGER, allowNull: true })
+  target_user_id: number;
+
+  @ForeignKey(() => Activity)
+  @Column({ type: DataType.INTEGER, allowNull: true })
+  target_activity_id: number;
+
+  @ForeignKey(() => InteractionType)
+  @Column({ type: DataType.INTEGER, allowNull: false })
+  interaction_type_id!: number;
+
+  @Column({ type: DataType.JSONB, allowNull: true })
+  metadata: Record<string, any>;
+}
