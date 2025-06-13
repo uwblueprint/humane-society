b27177058f773c9ff8a0b81569297eebd91188ae
Tony Qiu <90103972+tonyqiu123@users.noreply.github.com>
2025-06-13 13:50:30 -0400

    Update frontend/src/components/common/PageTitleUpdater.tsx

--- frontend/src/components/common/PageTitleUpdater.tsx
+++ frontend/src/components/common/PageTitleUpdater.tsx
@@ -50,7 +50,7 @@ const PageTitleUpdater: React.FC = () => {
 
   useEffect(() => {
     document.title = getPageTitle();
-  }, [location.pathname]);
+  }, [location.pathname, getPageTitle]);
 
   return null;
 };
