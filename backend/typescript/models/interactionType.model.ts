fc73d97eb5a6bae913df520dd5c5a4cc2d74b960
ludavidca <davidjylu7@gmail.com>
2025-06-16 21:30:32 -0400

    Not implemented foreign key handling yet, might look at it for next ticket

--- backend/typescript/models/interactionType.model.ts
+++ backend/typescript/models/interactionType.model.ts
@@ -1,51 +1,45 @@
-import {
-  Column,
-  DataType,
-  Model,
-  ForeignKey,
-  BelongsTo,
-  Table,
-  AllowNull,
-} from "sequelize-typescript";
+import { Column, DataType, Model, Table } from "sequelize-typescript";
 import { InteractionTypeEnum } from "../types";
 
 @Table({ tableName: "interaction_type" })
-export default class Interaction_Type extends Model {
-
-  @Column({ type: DataType.ENUM(
-    "Task Assigned",
-    "Task Self-Assigned",
-    "Task Unassigned",
-    "Task Started",
-    "Task Restarted",
-    "Task Completed",
-    "Task Incomplete",
-    "Task Inactive",
-    "Task Deleted",
-    "Task Assignee Changed",
-    "Task Instructions Changed",
-    "Task Start Date Changed",
-    "Task End Date Changed",
-    "Recurring Task Deleted",
-    "Recurring Task Name Changed",
-    "Recurring Task Days Changed",
-    "Recurring Task Cadence Changed",
-    "User Name Changed",
-    "User Color Level Changed",
-    "User Role Changed",
-    "User Invited",
-    "User Deleted",
-    "Pet Deleted",
-    "Pet Name Changed",
-    "Pet Color Level Changed",
-    "Pet Neuter Status Changed",
-    "Pet Safety Info Changed",
-    "Pet Medical Info Changed",
-    "Pet Management Info Changed",
-    "Task Template Deleted",
-    "Task Template Name Changed",
-    "Task Template Instructions Changed",
-  ), allowNull: false })
+export default class InteractionType extends Model {
+  @Column({
+    type: DataType.ENUM(
+      "Task Assigned",
+      "Task Self-Assigned",
+      "Task Unassigned",
+      "Task Started",
+      "Task Restarted",
+      "Task Completed",
+      "Task Incomplete",
+      "Task Inactive",
+      "Task Deleted",
+      "Task Assignee Changed",
+      "Task Instructions Changed",
+      "Task Start Date Changed",
+      "Task End Date Changed",
+      "Recurring Task Deleted",
+      "Recurring Task Name Changed",
+      "Recurring Task Days Changed",
+      "Recurring Task Cadence Changed",
+      "User Name Changed",
+      "User Color Level Changed",
+      "User Role Changed",
+      "User Invited",
+      "User Deleted",
+      "Pet Deleted",
+      "Pet Name Changed",
+      "Pet Color Level Changed",
+      "Pet Neuter Status Changed",
+      "Pet Safety Info Changed",
+      "Pet Medical Info Changed",
+      "Pet Management Info Changed",
+      "Task Template Deleted",
+      "Task Template Name Changed",
+      "Task Template Instructions Changed",
+    ),
+    allowNull: false,
+  })
   action_type!: InteractionTypeEnum;
 
   @Column({ type: DataType.STRING, allowNull: false })
