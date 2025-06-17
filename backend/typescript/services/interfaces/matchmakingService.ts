ccd407304e30e09f27d4ff053a89dbcabc4b8a73
yut-code <yut3335@gmail.com>
2025-06-16 21:16:54 -0400

    endpoints

---
+++ backend/typescript/services/interfaces/matchmakingService.ts
@@ -0,0 +1,9 @@
+import { PetResponseDTO } from "../implementations/petService";
+import { UserResponseDTO } from "../implementations/userService";
+
+export interface IMatchmakingService {
+    /* eslint-disable class-methods-use-this */
+    getMatchingPetsForUser(userId: string): Promise<PetResponseDTO[]>;
+    getMatchingUsersForPet(petId: string): Promise<UserResponseDTO[]>;
+}
+
