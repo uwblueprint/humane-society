d7ed77c99a2767207cdfb50005994c533626435e
tonyqiu123 <tonyqiu12345@gmail.com>
2025-06-18 14:08:25 -0400

    remove unnecessary code

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -18,7 +18,7 @@ module.exports = {
     "import/resolver": {
       node: {
         extensions: [".js", ".jsx", ".ts", ".tsx"],
-        moduleDirectory: ["node_modules", "src/"],
+        moduleDirectory: ["src/"],
       },
     },
   },
