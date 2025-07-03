9c80c17e8f6a68bbb234f3632f47fde43ce088e1
yut-code <yut3335@gmail.com>
2025-07-03 19:55:26 -0400

    Update matchmakingService.ts

--- backend/typescript/services/interfaces/matchmakingService.ts
+++ backend/typescript/services/interfaces/matchmakingService.ts
@@ -1,8 +1,8 @@
 import { PetResponseDTO } from "../interfaces/petService";
-import { UserResponseDTO } from "../interfaces/userService";
+import { UserDTO } from "../../types";
 
 export interface IMatchmakingService {
   /* eslint-disable class-methods-use-this */
   getMatchingPetsForUser(userId: string): Promise<PetResponseDTO[]>;
-  getMatchingUsersForPet(petId: string): Promise<UserResponseDTO[]>;
+  getMatchingUsersForPet(petId: string): Promise<UserDTO[]>;
 }
