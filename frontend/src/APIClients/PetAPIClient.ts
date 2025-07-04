6dadf387e4664260036b36a44d3a624f42df1c36
Mehulio123 <136765802+Mehulio123@users.noreply.github.com>
2025-07-03 20:48:13 -0400

    PetAPI

---
+++ frontend/src/APIClients/PetAPIClient.ts
@@ -0,0 +1,20 @@
+import baseAPIClient from "./BaseAPIClient";
+import AUTHENTICATED_USER_KEY from "../constants/AuthConstants";
+import { getLocalStorageObjProperty } from "../utils/LocalStorageUtils";
+
+const getPetTasks = async (petId: number): Promise<Task[]> => {
+  const bearerToken = `Bearer ${getLocalStorageObjProperty(
+    AUTHENTICATED_USER_KEY,
+    "accessToken",
+  )}`;
+  try {
+    const { data } = await baseAPIClient.get(`/pets/${petId}/tasks`, {
+      headers: { Authorization: bearerToken },
+    });
+    return data;
+  } catch (error) {
+    throw new Error(`Failed to fetch pet tasks: ${error}`);
+  }
+};
+
+export default { getPetTasks };
