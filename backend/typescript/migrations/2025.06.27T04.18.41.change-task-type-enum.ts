1237b9d779d9a3f9e1159dc45d2a3274140dcb37
zhusophia <iceyes123@gmail.com>
2025-07-07 21:01:42 -0400

    Deleted category and removed check

--- backend/typescript/migrations/2025.06.27T04.18.41.change-task-type-enum.ts
+++ backend/typescript/migrations/2025.06.27T04.18.41.change-task-type-enum.ts
@@ -1,61 +1,43 @@
-import { Migration } from '../umzug';
-import { TaskType } from "../types";
 import { DataType } from "sequelize-typescript";
+import { Migration } from "../umzug";
+import { TaskType } from "../types";
 
-const TASKTEMPLATES_TABLE = 'task_templates'
-const TASK_TYPE = 'task_type'
+const TASKTEMPLATES_TABLE = "task_templates";
+const TASK_TYPE = "task_type";
 
 export const up: Migration = async ({ context: sequelize }) => {
-    const queryinterface = sequelize.getQueryInterface()
-    // remove column so we can change type 
-    await queryinterface.removeColumn(
-        TASKTEMPLATES_TABLE, 
-        TASK_TYPE
-    );
+  const queryinterface = sequelize.getQueryInterface();
+  // remove column so we can change type
+  await queryinterface.removeColumn(TASKTEMPLATES_TABLE, TASK_TYPE);
 
-    //make sure this type does not already exist
+  // make sure this type does not already exist
 
-    //    await queryinterface.sequelize.query(
-    //      'DROP TYPE IF EXISTS "enum_task_templates_task_type";'
-    //);
+  //    await queryinterface.sequelize.query(
+  //      'DROP TYPE IF EXISTS "enum_task_templates_task_type";'
+  // );
 
-    // add column with updated tasktype defintion
-    await queryinterface.addColumn(
-        TASKTEMPLATES_TABLE, 
-        TASK_TYPE, 
-        {
-            type: DataType.ENUM(...Object.values(TaskType)), 
-            allowNull: false, 
-            defaultValue: TaskType.MISC
-
-        }
-    );
-
-
+  // add column with updated tasktype defintion
+  await queryinterface.addColumn(TASKTEMPLATES_TABLE, TASK_TYPE, {
+    type: DataType.ENUM(...Object.values(TaskType)),
+    allowNull: false,
+    defaultValue: TaskType.MISC,
+  });
 };
-export const down: Migration = async ({ context: sequelize })  => {
-        const queryinterface = sequelize.getQueryInterface()
+export const down: Migration = async ({ context: sequelize }) => {
+  const queryinterface = sequelize.getQueryInterface();
 
-       // remove column to change type
-    await queryinterface.removeColumn(
-        TASKTEMPLATES_TABLE, 
-        TASK_TYPE
-    );
+  // remove column to change type
+  await queryinterface.removeColumn(TASKTEMPLATES_TABLE, TASK_TYPE);
 
-    // drop the type since you can't change the type if it already exists
-    await queryinterface.sequelize.query(
-        'DROP TYPE IF EXISTS "enum_task_templates_task_type";'
-    );
+  // drop the type since you can't change the type if it already exists
+  await queryinterface.sequelize.query(
+    'DROP TYPE IF EXISTS "enum_task_templates_task_type";',
+  );
 
-    // adds column to change type
-    await queryinterface.addColumn(
-        TASKTEMPLATES_TABLE, 
-        TASK_TYPE, 
-        {
-            type: DataType.ENUM(...Object.values(TaskType)), 
-            allowNull: false, 
-            defaultValue: TaskType.MISC
-        }
-    );
-
+  // adds column to change type
+  await queryinterface.addColumn(TASKTEMPLATES_TABLE, TASK_TYPE, {
+    type: DataType.ENUM(...Object.values(TaskType)),
+    allowNull: false,
+    defaultValue: TaskType.MISC,
+  });
 };
