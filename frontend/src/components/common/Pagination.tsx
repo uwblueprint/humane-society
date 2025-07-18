12e2f05b70221421132a7c4271ea54f698a22e07
Tony Qiu <tonyqiu@Mac-mini.local>
2025-07-18 10:20:23 -0400

    linting

--- frontend/src/components/common/Pagination.tsx
+++ frontend/src/components/common/Pagination.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from "react";
+import React, { FC } from "react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import {
   Flex,
@@ -24,86 +24,90 @@ const Pagination: FC<PaginationProps> = ({
   itemsPerPage,
   className = "",
 }) => {
-  const [curPageNumber, setCurPageNumber] = useState(value);
   const MAX_NUM_OF_PAGES = ceil(numberOfItems / itemsPerPage);
   const theme = useTheme();
 
-  const onPageChange = (newPageNumber: number) => {
-    if (newPageNumber < 1) {
-      setCurPageNumber(1);
-    } else if (newPageNumber > MAX_NUM_OF_PAGES) {
-      setCurPageNumber(MAX_NUM_OF_PAGES);
+  const handlePrevClick = () => {
+    if (value > 1) {
+      onChange(value - 1);
     } else {
-      setCurPageNumber(newPageNumber);
-      onChange(newPageNumber);
+      onChange(1);
     }
   };
 
-  const onInputChange = (newPageNumber: number) => {
-    setCurPageNumber(newPageNumber);
+  const handleNextClick = () => {
+    if (value < MAX_NUM_OF_PAGES) {
+      onChange(value + 1);
+    } else {
+      onChange(MAX_NUM_OF_PAGES);
+    }
+  };
+
+  const handleInputChange = (val: string) => {
+    const newPageNumber = Number(val);
     onChange(newPageNumber);
   };
 
   return (
     <Flex
+      width="100%"
+      maxWidth="25rem"
       alignItems="center"
-      gap="1.5rem"
       justifyContent="space-between"
-      width="25rem"
       alignSelf="center"
       className={className}
     >
       <ChevronLeft
         size={30}
-        color={
-          curPageNumber <= 1 ? theme.colors.gray[300] : theme.colors.gray[700]
-        }
-        onClick={() => onPageChange(curPageNumber - 1)}
+        color={value <= 1 ? theme.colors.gray[300] : theme.colors.gray[700]}
+        onClick={handlePrevClick}
       />
-      <Text
-        m={0}
-        fontSize="18px"
-        color={theme.colors.gray[700]}
-        fontWeight="400"
-      >
-        Page
-      </Text>
-      <NumberInput
-        onChange={(val) => onInputChange(Number(val))}
-        inputMode="numeric"
-        value={curPageNumber}
-        max={MAX_NUM_OF_PAGES}
-        min={1}
-        clampValueOnBlur={true}
-        defaultValue={1}
-        size="sm"
-        focusBorderColor={theme.colors.gray[500]}
-        textColor="gray.700"
-        fontWeight="400"
-      >
-        <NumberInputField
-          height="2.5rem"
-          padding="0.35rem"
-          maxW="3rem"
-          alignContent="center"
-          borderColor={theme.colors.gray[500]}
-          borderRadius="0.35rem"
-          textAlign="center"
-        />
-      </NumberInput>
-      <Text
-        m={0}
-        fontSize="18px"
-        color={theme.colors.gray[700]}
-        fontWeight="400"
-      >
-        of {MAX_NUM_OF_PAGES}
-      </Text>
+      <Flex gap="1.25rem" alignItems="center">
+        <Text
+          m={0}
+          fontSize="18px"
+          color={theme.colors.gray[700]}
+          fontWeight="400"
+        >
+          Page
+        </Text>
+        <NumberInput
+          onChange={handleInputChange}
+          inputMode="numeric"
+          value={value}
+          max={MAX_NUM_OF_PAGES}
+          min={1}
+          clampValueOnBlur
+          defaultValue={1}
+          size="sm"
+          focusBorderColor={theme.colors.gray[500]}
+          textColor="gray.700"
+          fontWeight="400"
+        >
+          <NumberInputField
+            height="2.5rem"
+            padding="0.35rem"
+            maxW="3rem"
+            alignContent="center"
+            borderColor={theme.colors.gray[500]}
+            borderRadius="0.35rem"
+            textAlign="center"
+          />
+        </NumberInput>
+        <Text
+          m={0}
+          fontSize="18px"
+          color={theme.colors.gray[700]}
+          fontWeight="400"
+        >
+          of {MAX_NUM_OF_PAGES}
+        </Text>
+      </Flex>
       <ChevronRight
         size={30}
-        onClick={() => onPageChange(curPageNumber + 1)}
+        onClick={handleNextClick}
         color={
-          curPageNumber >= MAX_NUM_OF_PAGES
+          value >= MAX_NUM_OF_PAGES
             ? theme.colors.gray[300]
             : theme.colors.gray[700]
         }
