aca4eab519a9b789c99ec6d5cb2fba7285669328
sthuray <138075787+sthuray@users.noreply.github.com>
2025-09-28 20:52:53 -0400

    Correct linting after changes

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -38,5 +38,5 @@ module.exports = {
     "jsx-a11y/click-events-have-key-events": "off",
     "jsx-a11y/no-static-element-interactions": "off",
   },
-  ignorePatterns: ["build/*", ".eslintrc.js"],
+  ignorePatterns: ["build/*", ".eslintrc.js", "starter_code_reference/**"],
 };
