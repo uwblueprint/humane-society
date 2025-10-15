ce0ae59bb06197e5688728f8915675745959ea50
Raj Shah <rajnshah2020@gmail.com>
2025-10-14 20:21:31 -0400

    create frontend to test

--- frontend/src/pages/AdminPage.tsx
+++ frontend/src/pages/AdminPage.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Flex, Text } from "@chakra-ui/react";
 import MainPageButton from "../components/common/MainPageButton";
+import CreateUserTestForm from "../components/user-management/CreateUserTestForm";
 
 const AdminPage = (): React.ReactElement => {
   return (
-    <Flex direction="column" align="center" w="25%" margin="0 auto">
-      <Text textStyle="h1" color="gray.700" m={0}>
+    <Flex direction="column" align="center" width="100%" margin="0 auto" py={8}>
+      <Text textStyle="h1" color="gray.700" mb={6}>
         Admin Page
       </Text>
+      <CreateUserTestForm />
       <MainPageButton />
     </Flex>
   );
