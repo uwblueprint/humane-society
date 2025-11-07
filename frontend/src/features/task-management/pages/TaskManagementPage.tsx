fe1a3745e2165c77d2c10b53fd09e047f076579e
Artyom-G <artyomgv05@gmail.com>
2025-11-06 20:21:51 -0500

    connecting task templates

--- frontend/src/features/task-management/pages/TaskManagementPage.tsx
+++ frontend/src/features/task-management/pages/TaskManagementPage.tsx
@@ -1,17 +1,21 @@
-import React, { useState, useMemo } from "react";
+import React, { useState, useMemo, useEffect } from "react";
 import { Flex } from "@chakra-ui/react";
 import { useHistory } from "react-router-dom";
 import TaskManagementTable from "../components/TaskManagementTable";
 import Filter from "../../../components/common/Filter";
 import Search from "../../../components/common/Search";
-import { mockTasks } from "../../../types/TaskTypes";
+import { type Task } from "../../../types/TaskTypes";
 import Button from "../../../components/common/Button";
 import { ADD_TASK_TEMPLATE_PAGE } from "../../../constants/Routes";
+import TaskTemplateAPIClient from "../../../APIClients/TaskTemplateAPIClient";
 
 const TaskManagementPage = (): React.ReactElement => {
   const history = useHistory();
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const [tasks, setTasks] = useState<Task[]>([]);
   const [filters, setFilters] = useState<Record<string, string[]>>({});
   const [search, setSearch] = useState<string>("");
+
 
   const handleClearFilters = () => {
     setFilters({});
@@ -31,8 +35,9 @@ const TaskManagementPage = (): React.ReactElement => {
   };
 
   const filteredTasks = useMemo(() => {
-    return mockTasks
-      .filter((task) => {
+
+    return tasks
+      .filter((task: Task) => {
         return Object.keys(filters).every((key) => {
           const filterVals = filters[key];
           if (!filterVals || filterVals.length === 0) return true;
@@ -40,11 +45,27 @@ const TaskManagementPage = (): React.ReactElement => {
         });
       })
       .filter(
-        (task) =>
+        (task: Task) =>
           task.name.toLowerCase().includes(search.toLowerCase()) ||
           task.instructions.toLowerCase().includes(search.toLowerCase()),
       );
   }, [filters, search]);
+
+  const getTasks = async () => {
+    try {
+      const fetchedTasks = await TaskTemplateAPIClient.getAllTaskTemplates();
+
+      if (fetchedTasks != null) {
+        setTasks(fetchedTasks);
+      }
+    } catch (error) {
+      setErrorMessage(`${error}`);
+    }
+  };
+
+  useEffect(() => {
+    getTasks();
+  }, []);
 
   return (
     <Flex direction="column" gap="2rem" pt="2rem">
