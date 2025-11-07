94bf494d0d3c7b80abcea929319b1ee546ae6416
Cindy Li <c45li@uwaterloo.ca>
2025-11-06 21:00:31 -0500

    added interaction log messages and type enums

--- backend/typescript/types.ts
+++ backend/typescript/types.ts
@@ -111,5 +111,48 @@ export enum InteractionTypeEnum {
   TASK_ASSIGNEE_CHANGED = "TASK_ASSIGNEE_CHANGED",
   USER_ROLE_CHANGED = "USER_ROLE_CHANGED",
   PET_NAME_CHANGED = "PET_NAME_CHANGED",
-  //fill in rest from table
+
+  // Task Status
+  ASSIGNED_TASK = "Assigned Task",
+  SELF_ASSIGNED_TASK = "Self-Assigned Task",
+  UNASSIGNED_TASK = "Unassigned Task",
+  STARTED_TASK = "Started Task",
+  RESTARTED_TASK = "Restarted Task",
+  COMPLETED_TASK = "Completed Task",
+  MARKED_TASK_INCOMPLETE = "Incomplete Task",
+  MARKED_TASK_INACTIVE = "Inactive Task",
+
+  // Task Details
+  DELETED_TASK = "Deleted Task",
+  CHANGED_TASK_ASSIGNEE = "Changed Task Assignee",
+  CHANGED_TASK_INSTRUCTIONS = "Changed Task Instructions",
+  CHANGED_TASK_START_DATE = "Changed Task Start Date",
+  CHANGED_TASK_END_DATE = "Changed Task End Date",
+  DELETED_RECURRING_TASK = "Deleted Recurring Task",
+  CHANGED_RECURRING_TASK_NAME = "Changed Recurring Task Name",
+  CHANGED_RECURRING_TASK_DAYS = "Changed Recurring Task Days",
+  CHANGED_RECURRING_TASK_CADENCE = "Changed Recurring Task Cadence",
+
+  // Personal Details
+  CHANGED_USER_NAME = "Changed User Name",
+  CHANGED_USER_COLOR_LEVEL = "Changed User Color Level",
+  CHANGED_USER_ROLE = "Changed User Role",
+
+  // User Details
+  INVITED_USER = "Invited User",
+  DELETED_USER = "Deleted User",
+
+  // Pet Details
+  DELETED_PET = "Deleted Pet",
+  CHANGED_PET_NAME = "Changed Pet Name",
+  CHANGED_PET_COLOR_LEVEL = "Changed Pet Color Level",
+  CHANGED_PET_NEUTER_STATUS = "Changed Pet Neuter Status",
+  CHANGED_PET_SAFETY_INFO = "Changed Pet Safety Info",
+  CHANGED_PET_MEDICAL_INFO = "Changed Pet Medical Info",
+  CHANGED_PET_MANAGEMENT_INFO = "Changed Pet Management Info",
+
+  // Task Template Details
+  DELETED_TASK_TEMPLATE = "Deleted Task Template",
+  CHANGED_TASK_TEMPLATE_NAME = "Changed Task Template Name",
+  CHANGED_TASK_TEMPLATE_INSTRUCTIONS = "Changed Task Template Instructions",
 }
