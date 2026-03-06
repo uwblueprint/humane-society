02ecc1f5b83cf5fff2116793f3a4dec500347c45
daphnehlj <daphnehlj25@gmail.com>
2026-03-05 21:09:24 -0500

    add task form types for page 2

--- frontend/src/features/pet-profile/components/add-task-form/AddTaskFormTypes.ts
+++ frontend/src/features/pet-profile/components/add-task-form/AddTaskFormTypes.ts
@@ -1,6 +1,30 @@
 import { Task } from "../../../../types/TaskTypes";
 
 export interface AddTaskFormData {
+
+  // page 1
   search: string;
   selectedTemplate: Task | null;
+
+
+  // page 2
+  taskName: string;
+  taskCategory: string;
+  instructions: string;
+
+  startDateDay: string;
+  startDateMonth: string;
+  startDateYear: string;
+  startTimeMinute: string;
+  startTimeHour: string;
+  endTimeMinute: string;
+  endTimeHour: string;
+  
+  isRepeating: boolean;
+  recurringDays: string[];
+  recurringCadence: string;
+  endDateDay: string;
+  endDateMonth: string;
+  endDateYear: string;
+
 }
