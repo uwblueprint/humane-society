9bab0965097af9f9054ab028ad90e83913985e17
VictorChan880 <victorchan870@gmail.com>
2026-03-09 05:11:42 -0400

    added upload photo flow

--- frontend/src/utils/CommonUtils.ts
+++ frontend/src/utils/CommonUtils.ts
@@ -64,3 +64,7 @@ export const getAgeInMonths = (birthday: string): number => {
 
   return totalMonths;
 };
+
+export const MAX_FILE_SIZE_MB = 5;
+export const MAX_FILE_SIZE_BYTES = MAX_FILE_SIZE_MB * 1024 * 1024;
+export const ACCEPTED_TYPES = ["image/png", "image/jpeg", "image/jpg"];
