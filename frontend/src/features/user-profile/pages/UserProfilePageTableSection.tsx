dcfd7cbf7dc2297dc53182355b2588df5d93f317
zeukyr <angelayxli@gmail.com>
2026-06-29 13:19:31 -0400

    move onClick function to the entire row

--- frontend/src/features/user-profile/pages/UserProfilePageTableSection.tsx
+++ frontend/src/features/user-profile/pages/UserProfilePageTableSection.tsx
@@ -11,38 +11,31 @@ interface UserProfilePageTableSectionProps {
   gridTemplateColumns: string;
 }
 
+const getStatusBadge = (task: ScheduledTaskDTO) => {
+  let label = "Assigned";
+  if (task.scheduledStartTime) {
+    const d = new Date(task.scheduledStartTime);
+    const midnight = new Date(d.getFullYear(), d.getMonth(), d.getDate() + 1);
+
+    if (task.endTime && new Date(task.endTime) < midnight) {
+      label = "Completed";
+    } else if (new Date() >= midnight) {
+      label = "Incomplete";
+    }
+  }
+
+  return (
+    <Button as="button" variant="gray-shaded" size="medium" type="button">
+      {label}
+    </Button>
+  );
+};
+
 const UserProfilePageTableSection = ({
   tasks,
   gridTemplateColumns,
 }: UserProfilePageTableSectionProps): React.ReactElement => {
   const history = useHistory();
-
-  const getStatusBadge = (task: ScheduledTaskDTO) => {
-    let label = "Assigned";
-    if (task.scheduledStartTime) {
-      const d = new Date(task.scheduledStartTime);
-      const midnight = new Date(d.getFullYear(), d.getMonth(), d.getDate() + 1);
-
-      if (task.endTime && new Date(task.endTime) < midnight) {
-        label = "Completed";
-      } else if (new Date() >= midnight) {
-        label = "Incomplete";
-      }
-    }
-
-    return (
-      <Button
-        as="button"
-        variant="gray-shaded"
-        size="medium"
-        type="button"
-        onClick={() => history.push(`/pet-profile/${task.petId}`)}
-      >
-        {label}
-      </Button>
-    );
-  };
-
   return (
     <Flex direction="column">
       {tasks.map((task) => (
@@ -57,6 +50,7 @@ const UserProfilePageTableSection = ({
           marginBottom="0.5rem"
           marginTop="0.5rem"
           borderRadius="0.75rem"
+          onClick={() => history.push(`/pet-profile/${task.petId}`)}
         >
           <Flex align="center" gap="0.75rem">
             <Icon as={taskCategoryIcons[task.category]} boxSize="1.5rem" />
